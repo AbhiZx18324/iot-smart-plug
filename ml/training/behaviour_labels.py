ea64ab90190a3b281c268635d2cb04e7d1a76bb9
AbhiZx18324 <abhirup.adhikary2004@gmail.com>
2026-02-19 18:56:04 +0530

    appliance -> behaviour mapping

---
+++ ml/training/behaviour_labels.py
@@ -0,0 +1,17 @@
+BEHAVIOR_LABEL_MAP = {
+    "Fan": "MOTOR_VARIABLE",
+    "Vacuum": "MOTOR_VARIABLE",
+    "Washing Machine": "MOTOR_VARIABLE",
+    "Hairdryer": "MOTOR_VARIABLE",
+
+    "Fridge": "COMPRESSOR_CYCLIC",
+    "Air Conditioner": "COMPRESSOR_CYCLIC",
+
+    "Heater": "RESISTIVE_HEATING",
+    "Incandescent Light Bulb": "RESISTIVE_HEATING",
+
+    "Microwave": "PULSED_ELECTRONICS",
+
+    "Laptop": "BASE_LOAD",
+    "Compact Fluorescent Lamp": "BASE_LOAD"
+}
